9e70eec20140da13d64583206bd8d084ca0c4cce
chaima.ghaddab <chaima.ghaddab@tum.de>
2023-11-18 16:15:08 +0100

    summarization working with both assistant +

--- app.py
+++ app.py
@@ -28,20 +28,14 @@ def compare_code(main_code, compare_code):
     similarity = cmp.compare(main_code, compare_code)
     return jsonify({'similarity': similarity})
 
-@app.route('/summarize/<code>', methods=['GET'])
-def summarize(code):
+@app.route('/summarize-assistant/<code>', methods=['GET'])
+def summarize_assistant(code):
   """
     ---
     parameters:
       - name: code
         in: path
-        schema:
-          type: object
-          properties:
-            code:
-              type: string
-        example:
-          code: "your_code_here"
+        type: string
         required: true
         description: yes
     responses:
@@ -50,7 +44,27 @@ def summarize(code):
   """
   summarizer = CodeSummarizer()
   summary = summarizer.summarizeCode(code)
+  return jsonify(summary)
+  
+
+@app.route('/summarize-complettion/<code>', methods=['GET'])
+def summarize_completion(code):
+  """
+    ---
+    parameters:
+      - name: code
+        in: path
+        type: string
+        required: true
+        description: yes
+    responses:
+      200:
+        description: A code summarizer.
+  """
+  summarizer = CodeSummarizer()
+  summary = summarizer.summarize_code(code)
   print(type(summary))
+
   return jsonify({'similarity': summary})
 if __name__ == '__main__':
     app.run()
